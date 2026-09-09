5fdd3093fe3f81ca38ea28fea270c1684cd515a8
Mikhail <victor.belmont89@gmail.com>
2026-09-09 23:20:25 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://tobeewitched.ru',
-  
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
-  
   integrations: [react(), markdoc(), keystatic(), sitemap()]
 });
