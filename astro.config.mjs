27a7a5a7fba0185d7f4481bd8c882536c4cc9084
Mikhail <victor.belmont89@gmail.com>
2026-09-09 23:15:10 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,9 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  // Твой домен для карты сайта
   site: 'https://tobeewitched.ru',
-  
-  // Тот самый переходник для Vercel, который я стер
-  adapter: vercel(),
   
   vite: {
     plugins: [tailwindcss()]
